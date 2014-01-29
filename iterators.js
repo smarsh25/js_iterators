8af5a39dee281231c1a6c137f2c0eaf41f56f706
Alex Notov <anotov@gmail.com>
2014-01-28 18:41:37 -0800

    Update iterators.js

--- iterators.js
+++ iterators.js
@@ -1,8 +1,8 @@
 // create a namespace for our iterator functions
 var Iterators = (function() {
   return {
-    _each: function (arr, action) {
-      // implment the _each iterator
+    each: function (arr, action) {
+      // implment the each iterator
       // such that action is applied
       // to each element on the array
       // to which this function was applied
@@ -12,8 +12,8 @@ var Iterators = (function() {
       // return the original array
       return arr;
     },
-    _map: function(arr, action) {
-      // implement _map iterator
+    map: function(arr, action) {
+      // implement map iterator
       // such that action is applied
       // to each element on the array
       // returning a new array containing
@@ -34,32 +34,32 @@ var arr = [1,2,3];
 // 1
 // 2
 // 3
-console.log("results of applying Iterators._each to arr:");
-Iterators._each(arr, function (number) {
+console.log("results of applying Iterators.each to arr:");
+Iterators.each(arr, function (number) {
   console.log(number);
 });
 
 // should output
 // [2, 4, 6]
-console.log("results of applying Iterators._map to arr:");
+console.log("results of applying Iterators.map to arr:");
 console.log(
-  Iterators._map(arr, function (number) {
+  Iterators.map(arr, function (number) {
     return number * 2;
   })
 );
 
 // should output
 // false to indicate that the array returned
-// by _map is a different object than the array
+// by map is a different object than the array
 // that had been passed in
 console.log("arr and the array returned by map are identical:");
 console.log(
-  arr === Iterators._map(arr, function (number) {
+  arr === Iterators.map(arr, function (number) {
     return number * 2;
   })
 );
 
-// should output arr's contents to indicate that _map did not 
+// should output arr's contents to indicate that map did not 
 // modify the original array's contents
 console.log("arr's contents:");
 console.log(arr);
